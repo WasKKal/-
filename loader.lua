010269336d3c7051cb9c1db66884030eb53bcbf2
WasKKal <kkalalwe@icloud.com>
2026-02-12 21:46:30 +0800

    Add support for new PlaceId with corresponding script

--- loader.lua
+++ loader.lua
@@ -1,4 +1,3 @@
--- 自动识别直装版 · 检测到服务器直接运行脚本
 local PlaceId = game.PlaceId
 
 -- 在后巷
@@ -7,4 +6,7 @@ if PlaceId == 11257760806 then
 -- 割草模拟器
 elseif PlaceId == 133086043677134 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/%E5%89%B2%E8%8D%89%E6%A8%A1%E6%8B%9F%E5%99%A8.lua"))()
+-- 滴管增量
+elseif PlaceId == 70960300100792 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/%E6%B5%8C%E7%AE%A1%E5%A2%9E%E9%87%8F.lua"))()
 end
