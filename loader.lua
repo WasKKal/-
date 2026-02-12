66f4e0efe39059c60a3ab1802a16d583bed614a6
WasKKal <kkalalwe@icloud.com>
2026-02-12 22:29:30 +0800

    Update loadstring URLs for game scripts

--- loader.lua
+++ loader.lua
@@ -2,11 +2,11 @@ local PlaceId = game.PlaceId
 
 -- 在后巷
 if PlaceId == 11257760806 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/%E5%9C%A8%E5%90%8E%E5%B7%B7.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/在后巷.lua"))()
 -- 割草模拟器
 elseif PlaceId == 133086043677134 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/%E5%89%B2%E8%8D%89%E6%A8%A1%E6%8B%9F%E5%99%A8.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/割草模拟器.lua"))()
 -- 滴管增量
 elseif PlaceId == 70960300100792 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/%E6%B5%8C%E7%AE%A1%E5%A2%9E%E9%87%8F.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/滴管增量.lua"))()
 end
