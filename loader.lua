fbadd805a4ce298dd59b526b2f612369d596edb2
WasKKal <kkalalwe@icloud.com>
2026-02-14 22:06:14 +0800

    Add new script for glider escape functionality

--- loader.lua
+++ loader.lua
@@ -9,4 +9,7 @@ elseif PlaceId == 133086043677134 then
 -- 滴管增量
 elseif PlaceId == 70960300100792 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/滴管增量.lua"))()
+-- +1速度滑翔翼逃生
+elseif PlaceId == 115442728708640 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/滑翔翼逃生.lua"))()
 end
