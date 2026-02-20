f53b009423aa1b446186ef1f35bcdb1fe9d20166
WasKKal <kkalalwe@icloud.com>
2026-02-20 19:05:46 +0800

    Fix PlaceId variable usage in loader.lua

--- loader.lua
+++ loader.lua
@@ -15,15 +15,13 @@ elseif PlaceId == 115442728708640 then
 -- 你更愿意
 elseif PlaceId == 133379826754141 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/你更愿意,但它会发生.lua"))()
-
-local PlaceId = game.PlaceId
-
 -- 造船寻宝
-if PlaceID == 537413528 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/造船寻宝.lua"))()  
+elseif PlaceId == 537413528 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/造船寻宝.lua"))()
 -- 刀片旋转
-    elseif PlaceID == 79311273910901 then
+elseif PlaceId == 79311273910901 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/刀片旋转.lua"))()
 end
+
 -- 垃圾中心用户你好,如果你想盗取我的脚本,请不要这样做,请直接联系我,我会将部分功能与思路发送给您,不要偷取脚本,感谢使用垃圾中心
 -- MyQQcode:1763356884,QQGroupchatcode:1085475284
