761fcf6a5be0f2772d1ddc53574362de0a4103d5
WasKKal <kkalalwe@icloud.com>
2026-02-20 18:59:22 +0800

    Refactor PlaceId checks to use 'if' statements

--- loader.lua
+++ loader.lua
@@ -4,22 +4,22 @@ local PlaceId = game.PlaceId
 if PlaceId == 11257760806 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/在后巷.lua"))()
 -- 割草模拟器
-elseif PlaceId == 133086043677134 then
+if PlaceId == 133086043677134 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/割草模拟器.lua"))()
 -- 滴管增量
-elseif PlaceId == 70960300100792 then
+if PlaceId == 70960300100792 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/滴管增量.lua"))()
 -- +1速度滑翔翼逃生
-elseif PlaceId == 115442728708640 then
+if PlaceId == 115442728708640 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/滑翔翼逃生.lua"))()
 -- 你更愿意
-elseif PlaceId == 133379826754141 then
+if PlaceId == 133379826754141 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/你更愿意,但它会发生.lua"))()
 -- 造船寻宝
-elseif PlaceID == 537413528 then
+if PlaceID == 537413528 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/造船寻宝.lua"))()  
 -- 刀片旋转
-elseif PlaceID == 79311273910901 then
+if PlaceID == 79311273910901 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/刀片旋转.lua"))()
 end
 -- 垃圾中心用户你好,如果你想盗取我的脚本,请不要这样做,请直接联系我,我会将部分功能与思路发送给您,不要偷取脚本,感谢使用垃圾中心
