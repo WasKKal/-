9e8135db5709ebd680075c701c5370d2b280b94b
WasKKal <kkalalwe@icloud.com>
2026-02-22 14:41:33 +0800

    Add fireball training script for specific PlaceId
    Added a new script for fireball training based on PlaceId.

--- loader.lua
+++ loader.lua
@@ -24,7 +24,10 @@ elseif PlaceId == 79311273910901 then
 -- 终极+1力量
 elseif PlaceId == 98291788885415 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/终极+1力量.lua"))()
+-- 火球训练
+elseif PlaceId == 129195078205390 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/火球训练.lua"))()
 end
 
--- 垃圾中心用户你好,如果你想盗取我的脚本,请不要这样做,请直接联系我,我会将部分功能与思路发送给您,不要偷取脚本,感谢使用垃圾中心
+-- 垃圾中心用户你好,如果你想盗取我的脚本,请直接联系我,我会将部分功能与思路发送给您,不要偷取脚本,感谢使用垃圾中心
 -- MyQQcode:1763356884,QQGroupchatcode:1085475284
