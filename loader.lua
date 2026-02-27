97959132e4b2128b927b64b496eade3a079fe25c
WasKKal <kkalalwe@icloud.com>
2026-02-27 13:03:11 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,9 +3,9 @@ local PlaceId = game.PlaceId
 -- 在后巷
 if PlaceId == 11257760806 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/在后巷.lua"))()
--- 割草模拟器
+-- 割草增量模拟器
 elseif PlaceId == 133086043677134 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/割草模拟器.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/割草增量.lua"))()
 -- 滴管增量,未混淆
 elseif PlaceId == 70960300100792 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WasKKal/-/refs/heads/main/滴管增量.lua"))()
